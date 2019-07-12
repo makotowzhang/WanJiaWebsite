b0efa18ba4c1b794bada8d1e388097dec8877cf4
makotowzhang <31465825+makotowzhang@users.noreply.github.com>
2019-07-12 20:01:11 +0800

    dev

--- Web/Controllers/FontPageController.cs
+++ Web/Controllers/FontPageController.cs
@@ -9,17 +9,56 @@ namespace WebSite.Controllers
     public class FontPageController : Controller
     {
         // GET: FontPage
+        /// <summary>
+        /// 主页
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// 企业介绍
+        /// </summary>
+        /// <returns></returns>
         public ActionResult EnterPriseIntroduction()
         {
             return View();
         }
 
+        /// <summary>
+        /// 新闻中心
+        /// </summary>
+        /// <returns></returns>
         public ActionResult NewsCenter()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 楼盘风采
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult BuildingStyle()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 营销网络
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult BusinessChannel()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 信息反馈
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult RecruitmentTalents()
         {
             return View();
         }
