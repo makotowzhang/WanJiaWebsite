23680776c263a894200e619fc0e5fde013433147
makotowzhang <31465825+makotowzhang@users.noreply.github.com>
2019-08-28 21:15:19 +0800

    dev

--- Model/EnumModel/DicGroupCode.cs
+++ Model/EnumModel/DicGroupCode.cs
@@ -9,53 +9,18 @@ namespace Model.EnumModel
     public enum  DicGroupCode
     {
         /// <summary>
-        /// 资料库类别
+        /// 楼盘类别
         /// </summary>
-        DataLibraryType=1,
+        BuildingType=1,
 
         /// <summary>
-        /// 物料类型
+        /// 楼盘标签
         /// </summary>
-        MaterielType=2,
+        BuildingTag=2,
 
         /// <summary>
-        /// 公告类别
+        /// 物业类型
         /// </summary>
-        NoticeType=3,
-
-        /// <summary>
-        /// 租借物品类型
-        /// </summary>
-        LeaseType=4,
-
-        /// <summary>
-        /// 估价目的
-        /// </summary>
-        ValuationObjective = 5,
-
-        /// <summary>
-        /// 估价方法
-        /// </summary>
-        ValuationMethods=6,
-
-        /// <summary>
-        /// 收费状态
-        /// </summary>
-        ChargeStatus=7,
-
-        /// <summary>
-        /// 评估对象
-        /// </summary>
-        AssessmentObject=8,
-
-        /// <summary>
-        /// 资产性质
-        /// </summary>
-        AssetsNature=9,
-
-        /// <summary>
-        /// 价值类型
-        /// </summary>
-        ValueType=10
+        BuildPropertyType=3
     }
 }
---
+++ Web/Controllers/WebManageController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class WebManageController : Controller
+    {
+        // GET: WebManage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BuildingManage()
+        {
+            return View();
+        }
+
+
+        public ActionResult UploadImg()
+        {
+            return Content("");
+        }
+    }
+}
